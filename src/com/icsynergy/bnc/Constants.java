d3d0ac99fe1c0c7f94000dd1f1efbc3b78553826
premita <premita.gaikwad@xoriant.com>
2016-08-31 12:46:44 +0530

    Moved the constants to Main class

--- src/com/icsynergy/bnc/Constants.java
+++ src/com/icsynergy/bnc/Constants.java
@@ -11,10 +11,6 @@ public class Constants {
         public static final String ISMLogin = "ISMLogin";
         public static final String RESupn = "RESUserPrincipalName";
         public static final String EMP_MNGR = "EmployeeManager";		
-		public static final String HR_PENDING= "HR Pending";
-        public static final String EMP= "EMP";
-        public static final String NEW_USER_STATE= "NEW_USER_STATE";
-        public static final String CURRENT_USER= "CURRENT_USER";
     }
 
     public class CoucheAttributes {
